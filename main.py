b128647f864a353d8a46c3af11260ee7d798bc5a
Levko <lbeniakh@kse.org.ua>
2022-11-30 14:07:59 +0200

    Losing congratulations

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import words_fetcher
 import random
 
 
-def congratulate_user():
+def congratulate_user(congratulation_word):
     print("=============================")
-    print("= Congratulations! You won! =")
+    print(f"= Congratulations! You {congratulation_word}! =")
     print("=============================")
     print(f"Your words are: {guesses}")
 
@@ -51,7 +51,7 @@ while not is_game_over():
         guessed += 1
         guesses.append(guess)
         if guessed == WORDS_TO_WIN:
-            congratulate_user()
+            congratulate_user("won")
             exit()
         print(f"That's right! {WORDS_TO_WIN - guessed} to go")
     else:
@@ -62,3 +62,4 @@ while not is_game_over():
             print(f"Remember, you have used a word {str(guesses)[1:-1]}")
         elif len(guesses) > 1:
             print(f"Remember, you have used such words: {str(guesses)[1:-1]}")
+congratulate_user("lose")
